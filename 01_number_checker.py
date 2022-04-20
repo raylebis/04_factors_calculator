be52dc182e46b04574b112054f5f167f5775f198
raylebis <100240815+raylebis@users.noreply.github.com>
2022-04-20 18:38:15 +1200

    factor calculator

---
+++ 01_number_checker.py
@@ -0,0 +1,35 @@
+
+# checks input is a number more than zero
+def num_check(question):
+    valid = False
+    while not valid:
+
+        error = "Please enter a number that is more than zero and less than 200"
+
+        try:
+
+            # ask user to enter a number
+            response = int(input(question))
+
+            # checks number is more than zero
+            if response < 201:
+                return response
+
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+
+# Main Routine goes here
+keep_going = ""
+while keep_going == "":
+    print()
+    var_integer = num_check("Enter Integer: ")
+    print()
+    
+    
+    
+ 
