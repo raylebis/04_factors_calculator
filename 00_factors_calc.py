602d9e9c43d4a2c0fd939eb2e16c4742057fabce
raylebis <100240815+raylebis@users.noreply.github.com>
2022-05-09 16:11:35 +1200

    Update 00_factors_calc.py

--- 00_factors_calc.py
+++ 00_factors_calc.py
@@ -52,14 +52,23 @@ def num_check(question):
         except ValueError:
             print(error)
 
-
-# gets factors, returns a sorted list    
+# gets factors, returns a sorted list
 def get_factors(num):
+    print('The factors of', num, 'are: ')
+    factor_list = []
+    for i in range(1, num+1):
+
+        if(num % i) == 0:
+
+            print(i, end=' ')
+            factor_list.append(i)
+
+            factor_list.sort
 
 # Main routine goes here
 
 # Heading
-    statement_generator("Factor Calculator", "*")
+statement_generator("Factor Calculator", "*")
 
 # Display instructions if user has not used the program before
 first_time = input("Press <enter> to see the instructions or any key to continue: ")
@@ -76,28 +85,33 @@ while keep_going == "":
     # ask user for number to be factored...
     var_to_factor = num_check("Number: ")
 
-    if var_to_factor != 1:
+    if var_to_factor !=1:
         factor_list = get_factors(var_to_factor)
     else:
         factor_list = ""
         comment = "One is UNITY! it only has one factor. Itself"
-
-    # comments for squares / primes
-    if len(factor_list) == 2:
+    
+    if (factor_list) == 2:
         comment = "{} is a prime number.".format(var_to_factor)
-    elif len(factor_list) % 2 == 1:
-        comment = "{} is a perfect square".format(var_to_factor)
 
-    # output factors and comment
+    elif var_to_factor ==  1:
+        print("1 is unity (Only has 1 factor, itself) ")
+
+    # Perfect square finder
+    
+    int_num = int(var_to_factor)
+    root = math.sqrt(int_num)
+
+    if int (root + 0.5) ** 2 == var_to_factor:
+        comment = ("{} is a perfect square".format(var_to_factor))
+    else:
+        comment = "{} is a prime number.".format(var_to_factor)
 
     # generate heading...
     if var_to_factor == 1:
         heading = "One is special..."
-    else:
-        comment = "Factors of {}".format(var_to_factor)
 
     # Output factors and comment
-    statement_generator(heading, "*")
     print()
     print(factor_list)
     print(comment)
@@ -108,11 +122,4 @@ while keep_going == "":
 
 print()
 print("Thank you for using the factors calculator")
-
-
-
-
-
-
-
 
